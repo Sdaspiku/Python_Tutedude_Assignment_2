6cb5a6faea3e86c5993cab01cb0252c195d1d555
Sdaspiku <das.souvikd@gmail.com>
2025-04-21 06:10:37 +0530

    Add files via upload

---
+++ Assignment_2_Task_1.py
@@ -0,0 +1,6 @@
+print("This program determines whether a given number is odd or even")
+n = int(input('Enter the number: '))
+if n%2==0:
+    print("It is an even number")
+else:
+    print("It is an odd number")
---
+++ Assignment_2_Task_2.py
@@ -0,0 +1,5 @@
+print("This program prints the sum of numbers from 1 to 50")
+sum = 0
+for i in range(1,50,1):
+    sum = sum + i
+print("The sum of first fifty numbers is: ",sum)
